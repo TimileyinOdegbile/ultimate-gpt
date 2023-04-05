0ccd3accc075d137636c8f81fba98cf5528f6fe5
Timileyin Odegbile <odegbiletimileyin11@gmail.com>
2023-04-05 23:55:42 +0100

    Landing Page using React and Vanilla CSS

---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './features.css';
+
+const Features = ({ title, text }) => {
+  return (
+    <div>
+      Features
+    </div>
+  )
+}
+
+export default Features
